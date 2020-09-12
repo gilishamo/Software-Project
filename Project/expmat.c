fbb7cb04bd8781f14c8efb69f9943d08ee825e5e
unknown <gilishamo@mail.tau.ac.il>
2020-09-12 19:55:56 +0300

    added struct for expNumOfEdgesMat

---
+++ Project/expmat.c
@@ -0,0 +1,66 @@
+#include "util.h"
+#include "expmat.h"
+
+void freeExpMat(struct _expmat*);
+void add_degreeExpMat(struct _expmat*, double, int);
+int getDegreeExpMat(struct _expmat*, int);
+double getExpNumOfEdgesExpMat(struct _expmat*, int, int);
+void multExpMat(struct _expmat*, int*, int, const double*, double*);
+
+expmat* allocate_expmat(int numOfVertices) {
+	expmat *mat;
+	int* degreeOfVertex;
+
+	mat = (expmat*)allocate_memory(1, sizeof(expmat));
+	degreeOfVertex = (int*)allocate_memory(numOfVertices, sizeof(int));
+
+	mat->degreeOfVertex = degreeOfVertex;
+	mat->sumOfDegrees = 0;
+
+	mat->free = &freeExpMat;
+	mat->add_degree = &add_degreeExpMat;
+	mat->getDegree = &getDegreeExpMat;
+	mat->getExpNumOfEdges = &getExpNumOfEdgesExpMat;
+	mat->mult = &multExpMat;
+
+	return mat;
+}
+
+
+void freeExpMat(struct _expmat* mat) {
+	free(mat->degreeOfVertex);
+	free(mat);
+}
+
+void add_degreeExpMat(struct _expmat* mat, double degree, int indexOfVertex) {
+	int* degrees = mat->degreeOfVertex;
+
+	*(degrees + indexOfVertex) = degree;
+	mat->sumOfDegrees += degree;
+}
+
+int getDegreeExpMat(struct _expmat* mat, int i) {
+	int* degrees = mat->degreeOfVertex;
+
+	return *(degrees + i);
+}
+
+double getExpNumOfEdgesExpMat(struct _expmat* mat, int i, int j) {
+	int degreeI = mat->degreeOfVertex[i], degreeJ = mat->degreeOfVertex[j];
+
+	return (double)degreeI * (double)degreeJ / (double)(mat->sumOfDegrees);
+}
+
+void multExpMat(struct _expmat* mat, int* verticesOfSub, int sizeOfSub, const double* vector, double* result) {
+	int i, j, *degrees = mat->degreeOfVertex, sumOfDegrees = mat->sumOfDegrees;
+	double sum, currValForRow;
+
+	for (i = 0; i < sizeOfSub; i++) {
+		sum = 0;
+		currValForRow = (double)*(degrees + verticesOfSub[i]) / (double)(sumOfDegrees);
+		for (j = 0; j < sizeOfSub; j++) {
+			sum += ((double)*(degrees + verticesOfSub[j]) * currValForRow) * *(vector + j);
+		}
+		*(result + i) = sum;
+	}
+}
